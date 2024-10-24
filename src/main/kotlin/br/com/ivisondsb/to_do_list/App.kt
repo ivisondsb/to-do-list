a64075fd99223347bca995d9d976e2cf98a7612f
ivisondsb <ivisonk0@gmail.com>
2024-10-24 19:02:19 -0300

    first commit

---
+++ src/main/kotlin/br/com/ivisondsb/to_do_list/App.kt
@@ -0,0 +1,11 @@
+package br.com.ivisondsb.to_do_list
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+    runApplication<App>(*args)
+}
