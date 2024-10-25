853d18f83e9c7be3f264e1a869499c82561e812a
ivisondsb <ivisonk0@gmail.com>
2024-10-25 19:03:11 -0300

    add null to Task attributes and initialize id

--- src/main/kotlin/br/com/ivisondsb/to_do_list/models/Task.kt
+++ src/main/kotlin/br/com/ivisondsb/to_do_list/models/Task.kt
@@ -1,6 +1,7 @@
 package br.com.ivisondsb.to_do_list.models
 
 import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
 import java.time.LocalDate
 
 @Entity
@@ -9,15 +10,16 @@ data class Task(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    val id: Long?,
+    val id: Long? = 0,
     @Column(name = "title")
     val title: String,
     @Column(name = "description")
-    val description: String,
-    @Column(name = "status_id")
+    @NotBlank
+    val description: String? = null,
+    @Column(name = "id_status")
     val status: Int = 1,
     @Column(name = "created_on")
     val createdOn: LocalDate = LocalDate.now(),
     @Column(name = "finished_on")
-    val finishedOn: LocalDate
+    val finishedOn: LocalDate? = null
 )
